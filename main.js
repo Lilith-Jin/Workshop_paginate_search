9da860a0f08c9d8ea3c86739cca20a9466ef3ec3
Lilith <li6253462@gmail.com>
2021-12-10 11:23:01 +0800

    fetch info

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   // Start !
+  const info = fetch("https://top-250-movies.herokuapp.com/api/v1/movies/top")
+  .then((res)=>{
+    // console.log(res.json());
+    return res.json();
+    
+  })
+  .then((info)=>{
+      console.log(info);
+    })  
+
+
+
 })
